6b9209d8655f8330d0ac60c4a91b3625f3649778
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-10-28 03:07:10 +0100

    feat(asyncIterator): add asyncIterator function

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,89 @@
+import type {ExecutionContext} from "ava";
 import test from "ava";
+import {asyncIterator} from "./index";
 
-test("placeholder test", t => t.true(true));
+test("asyncIterator(empty)", async t => {
+    t.true((await asyncIterator([]).next()).done);
+});
+
+async function* asyncGenerator123(): AsyncIterable<number> {
+    yield Promise.resolve(1);
+    yield Promise.resolve(2);
+    yield Promise.resolve(3);
+}
+
+function* generator123(): Iterable<number> {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+// eslint-disable-next-line sonarjs/no-identical-functions
+function* promiseGenerator123(): Iterable<Promise<number>> {
+    yield Promise.resolve(1);
+    yield Promise.resolve(2);
+    yield Promise.resolve(3);
+}
+
+async function verify123(t: ExecutionContext, it: AsyncIterator<number>): Promise<void> {
+    const r1 = await it.next();
+    t.false(r1.done);
+    t.is(r1.value, 1);
+    const r2 = await it.next();
+    t.false(r2.done);
+    t.is(r2.value, 2);
+    const r3 = await it.next();
+    t.false(r3.done);
+    t.is(r3.value, 3);
+    const r4 = await it.next();
+    t.true(r4.done);
+    t.is(r4.value, undefined);
+}
+
+test("asyncIterator(AsyncIterator<T>)", async t => {
+    await verify123(t, asyncIterator(asyncGenerator123()[Symbol.asyncIterator]()));
+});
+
+test("asyncIterator(Iterator<T>)", async t => {
+    await verify123(t, asyncIterator(generator123()[Symbol.iterator]()));
+});
+
+test("asyncIterator(Iterator<Promise<T>>)", async t => {
+    await verify123(t, asyncIterator(promiseGenerator123()[Symbol.iterator]()));
+});
+
+test("asyncIterator(Promise<AsyncIterator<T>>)", async t => {
+    await verify123(t, asyncIterator(Promise.resolve(asyncGenerator123()[Symbol.asyncIterator]())));
+});
+
+test("asyncIterator(Promise<Iterator<T>>)", async t => {
+    await verify123(t, asyncIterator(Promise.resolve(generator123()[Symbol.iterator]())));
+});
+
+test("asyncIterator(Promise<Iterator<Promise<T>>>)", async t => {
+    await verify123(t, asyncIterator(Promise.resolve(promiseGenerator123()[Symbol.iterator]())));
+});
+
+test("asyncIterator(AsyncIterable<T>)", async t => {
+    await verify123(t, asyncIterator(asyncGenerator123()));
+});
+
+test("asyncIterator(Iterable<T>)", async t => {
+    await verify123(t, asyncIterator(generator123()));
+});
+
+test("asyncIterator(Iterable<Promise<T>>)", async t => {
+    await verify123(t, asyncIterator(promiseGenerator123()));
+});
+
+test("asyncIterator(Promise<AsyncIterable<T>>)", async t => {
+    await verify123(t, asyncIterator(Promise.resolve(asyncGenerator123())));
+});
+
+test("asyncIterator(Promise<Iterable<T>>", async t => {
+    await verify123(t, asyncIterator(Promise.resolve(generator123())));
+});
+
+test("asyncIterator(Promise<Iterable<Promise<T>>>)", async t => {
+    await verify123(t, asyncIterator(Promise.resolve(promiseGenerator123())));
+});
--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import type {AsyncIterableLike} from "@softwareventures/async-iterable";
+import {hasProperty} from "unknown";
+import {asyncIterable} from "@softwareventures/async-iterable";
 
 export type AsyncIteratorLike<T> =
     | AsyncIterator<T>
@@ -6,3 +8,59 @@ export type AsyncIteratorLike<T> =
     | Promise<AsyncIterator<T>>
     | Promise<Iterator<T | Promise<T>>>
     | AsyncIterableLike<T>;
+
+export function asyncIterator<T>(iterator: AsyncIteratorLike<T>): AsyncIterator<T> {
+    return hasProperty(iterator, Symbol.asyncIterator)
+        ? iterator[Symbol.asyncIterator]()
+        : hasProperty(iterator, Symbol.iterator)
+        ? asyncIterable(iterator)[Symbol.asyncIterator]()
+        : hasProperty(iterator, "then")
+        ? fromPromiseOfIteratorLike(iterator)
+        : fromSimpleIterator(iterator);
+}
+
+type SimpleAsyncIteratorLike<T> = AsyncIterator<T> | Iterator<T | Promise<T>>;
+
+function fromSimpleIterator<T>(iterator: SimpleAsyncIteratorLike<T>): AsyncIterator<T> {
+    return {
+        next: async (): Promise<IteratorResult<T>> => {
+            const n = await iterator.next();
+            if (n.done === true) {
+                return {done: true, value: undefined};
+            } else {
+                return {value: await n.value};
+            }
+        }
+    };
+}
+
+type SimpleAsyncIteratorOrIterable<T> =
+    | SimpleAsyncIteratorLike<T>
+    | AsyncIterable<T>
+    | Iterable<T | Promise<T>>;
+
+function fromSimpleIteratorOrIterable<T>(
+    iterator: SimpleAsyncIteratorOrIterable<T>
+): AsyncIterator<T> {
+    return hasProperty(iterator, Symbol.asyncIterator)
+        ? iterator[Symbol.asyncIterator]()
+        : hasProperty(iterator, Symbol.iterator)
+        ? asyncIterable(iterator)[Symbol.asyncIterator]()
+        : fromSimpleIterator(iterator);
+}
+
+type PromiseOfAsyncIteratorLike<T> =
+    | Promise<AsyncIterator<T>>
+    | Promise<Iterator<T | Promise<T>>>
+    | Promise<AsyncIterable<T>>
+    | Promise<Iterable<T | Promise<T>>>;
+
+function fromPromiseOfIteratorLike<T>(iterator: PromiseOfAsyncIteratorLike<T>): AsyncIterator<T> {
+    let next = async (): Promise<IteratorResult<T>> => {
+        const it = iterator.then(it => fromSimpleIteratorOrIterable(it));
+        const internal = async (): Promise<IteratorResult<T>> => (await it).next();
+        next = internal;
+        return internal();
+    };
+    return {next: async () => next()};
+}
